8999d277bc4445f339289ffdd2a2a010740dc78f
j-james <jj@j-james.me>
2019-12-01 16:01:16 -0800

    Support for Jailbreak, Ryanquest, and Homestuck^2
    Partially addresses #5, SBaHJ needs further testing.
    Note that on panels with multiple choices - page 68 of Jailbreak for example - right arrow will choose the last option.

--- hscompanion.js
+++ hscompanion.js
@@ -8,7 +8,7 @@ var adv = url.pop();
 
 if (page === "story" || page === "problem-sleuth"){
 	adv = page;
-	page = 1; 
+	page = 1;
 }
 
 //console.log(adv + " " + page); //I use this for debugging, don't judge
@@ -20,60 +20,62 @@ hussiecomment.then(result => {
     requestCommentary();
 });
 
-//Time to request the commentary from the API 
+//Time to request the commentary from the API
 
 function requestCommentary() {
-	fetch('https://recordcrash.com:3141/' + adv + '/' + page)
-		.then(data => data.json())
-		.then(datajson => {
-			if ((datajson[0].commentary != null)){ //If there's actually commentary for that page, we try to create the container
-				//We create the div we're going to embed in the page based on homestuck.com standards
+	if (window.location.hostname != "homestuck2.com") { // Only load commentary on the Homestuck proper domain - prevents issues with Homestuck^2
+		fetch('https://recordcrash.com:3141/' + adv + '/' + page)
+			.then(data => data.json())
+			.then(datajson => {
+				if ((datajson[0].commentary != null)){ //If there's actually commentary for that page, we try to create the container
+					//We create the div we're going to embed in the page based on homestuck.com standards
 
-				var son = document.createElement("div");
-        son.id = "commentary";
-				son.className = "row bg-hs-gray bg-light-gray--md pad-b-md pad-b-lg--md pos-r";
-				var sonsson = document.createElement("div");
-				sonsson.className = "mar-x-auto disp-bl bg-hs-gray pad-t-lg";
-				sonsson.style = "max-width:650px;";
-				var commentary = document.createElement("p");
-				commentary.className = "o-story_text type-rg type-hs-small--md type-center line-caption line-copy--md pad-x-0 pad-x-lg--md pad-b-lg";
-        commentary.style = "white-space:pre-line;";
+					var son = document.createElement("div");
+					son.id = "commentary";
+					son.className = "row bg-hs-gray bg-light-gray--md pad-b-md pad-b-lg--md pos-r";
+					var sonsson = document.createElement("div");
+					sonsson.className = "mar-x-auto disp-bl bg-hs-gray pad-t-lg";
+					sonsson.style = "max-width:650px;";
+					var commentary = document.createElement("p");
+					commentary.className = "o-story_text type-rg type-hs-small--md type-center line-caption line-copy--md pad-x-0 pad-x-lg--md pad-b-lg";
+					commentary.style = "white-space:pre-line;";
 
-				//We add the commentary to it (Note to self: look into just doing a secure innerHTML so we can add images in the future)
-				commentary.textContent = datajson[0].commentary;
+					//We add the commentary to it (Note to self: look into just doing a secure innerHTML so we can add images in the future)
+					commentary.textContent = datajson[0].commentary;
 
-				//And now we add the commentary container to the div, the div to the other div, etc
-				sonsson.appendChild(commentary);
-				son.appendChild(sonsson);
-				document.getElementsByClassName("pos-r")[0].appendChild(son);
-			}
+					//And now we add the commentary container to the div, the div to the other div, etc
+					sonsson.appendChild(commentary);
+					son.appendChild(sonsson);
+					document.getElementsByClassName("pos-r")[0].appendChild(son);
+				}
 
-			if ((datajson[0].notes != null)){ //If there's actually notes for that page, we try to create the container
-				//We create the div we're going to embed in the page based on homestuck.com standards
+				if ((datajson[0].notes != null)){ //If there's actually notes for that page, we try to create the container
+					//We create the div we're going to embed in the page based on homestuck.com standards
 
-				var son = document.createElement("div");
-        son.id = "notes";
-				son.className = "row bg-hs-gray bg-light-gray--md pad-b-md pad-b-lg--md pos-r";
-				var sonsson = document.createElement("div");
-				sonsson.className = "mar-x-auto disp-bl bg-hs-gray pad-t-lg";
-				sonsson.style = "max-width:650px;";
-				var notes = document.createElement("p");
-				notes.className = "o-story_text type-rg type-hs-small--md type-center line-caption line-copy--md pad-x-0 pad-x-lg--md pad-b-lg";
-        notes.style = "white-space:pre-line;";
+					var son = document.createElement("div");
+					son.id = "notes";
+					son.className = "row bg-hs-gray bg-light-gray--md pad-b-md pad-b-lg--md pos-r";
+					var sonsson = document.createElement("div");
+					sonsson.className = "mar-x-auto disp-bl bg-hs-gray pad-t-lg";
+					sonsson.style = "max-width:650px;";
+					var notes = document.createElement("p");
+					notes.className = "o-story_text type-rg type-hs-small--md type-center line-caption line-copy--md pad-x-0 pad-x-lg--md pad-b-lg";
+					notes.style = "white-space:pre-line;";
 
-				//We add the notes to it (Note to self: stop repeating comments)
-				notes.textContent = datajson[0].notes;
+					//We add the notes to it (Note to self: stop repeating comments)
+					notes.textContent = datajson[0].notes;
 
-				//And now we add the notes container to the div, the div to the other div, etc
-				sonsson.appendChild(notes);
-				son.appendChild(sonsson);
-				document.getElementsByClassName("pos-r")[0].appendChild(son);
-			}
-		}) 
-		.catch(error => {
-			if (error instanceof TypeError)
-				console.log("[HOMESTUCK COMPANION] There is no commentary available for this page yet, but there may be in the future. Support the official book releases!");
-		}); //or if there's an error remove the container
+					//And now we add the notes container to the div, the div to the other div, etc
+					sonsson.appendChild(notes);
+					son.appendChild(sonsson);
+					document.getElementsByClassName("pos-r")[0].appendChild(son);
+				}
+			})
+			.catch(error => {
+				if (error instanceof TypeError)
+					console.log("[HOMESTUCK COMPANION] There is no commentary available for this page yet, but there may be in the future. Support the official book releases!");
+			}); //or if there's an error remove the container
+	}
 }
 
 //Keyboard controls
@@ -83,7 +85,7 @@ document.onkeydown = function(e) {
     	case 17: //either ctrl key
     		if(e.location == 2){ //restricts key to right ctrl
     			if(document.activeElement.id != "SBURBStage" && document.activeElement.type != "application/x-shockwave-flash") {
-            	document.getElementsByClassName("o_chat-log-btn")[0].click();
+            		document.getElementsByClassName("o_chat-log-btn")[0].click();
         		}
         	}
     		break;
